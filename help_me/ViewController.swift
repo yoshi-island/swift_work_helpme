f0a7b1bf200b42c5753ce5f8d23d9a77706f31d4
yoshi-island <yukaki525.pgbpoc@gmail.com>
2017-05-28 18:12:23 +0900

    Initial Commit

---
+++ help_me/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  help_me
+//
+//  Created by 吉原有華 on 5/28/17.
+//  Copyright © 2017 yoshiisland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
